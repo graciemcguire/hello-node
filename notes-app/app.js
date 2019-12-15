61b5f7ae82f921506c8a91db8c72bf6a932f31dd
github username <github email address>
2019-12-15 15:53:15 -0500

    fix

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,14 @@
 const getNotes = require('./notes')
-const validator = require('validator')
+// const validator = require('validator')
 const chalk = require('chalk')
 
-const notes = getNotes()
+const command = process.argv[2]
+//argv = argument vector
 
-console.log(notes);
-console.log(validator.isURL('test.com'));
-console.log(chalk.green('success!'));
+if(command === 'add'){
+  console.log('adding note');
+} else if (command === 'remove'){
+  console.log('removing note');
+} else {
+  console.log(chalk.bgRed('error!'));
+}
