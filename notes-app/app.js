37d6de0c689806c733589e7175d78239360270be
github username <github email address>
2019-12-15 13:15:20 -0500

    adds validator and chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,9 @@
 const getNotes = require('./notes')
-
+const validator = require('validator')
+const chalk = require('chalk')
 
 const notes = getNotes()
 
 console.log(notes);
+console.log(validator.isURL('test.com'));
+console.log(chalk.green('success!'));
